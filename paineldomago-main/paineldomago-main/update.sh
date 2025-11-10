d2c8d4df287167ae6c4329496fb74a54e7040962
Lohanmelo <157203000+LohanMelo7@users.noreply.github.com>
2025-11-10 11:27:12 -0300

    Delete paineldomago-main/paineldomago-main directory

--- paineldomago-main/paineldomago-main/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-reuse="\e[1m[\e[94m+\e[0m\e[1m]\e[94m"
-clear
-rm -r main.py
-curl -o main.py https://github.com/LohanMelo7/main/blob/main/paineldomago-main/paineldomago-main/main.py
-clear
-echo -e "$reuse Atualização concluída com sucesso."
